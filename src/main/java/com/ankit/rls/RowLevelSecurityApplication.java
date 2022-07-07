0ab7acfb95a9e8293b3676ddfddf44749dfc6f5c
ankitwasnik <ankit@emtech.com>
2022-07-07 13:36:55 +0530

    init

---
+++ src/main/java/com/ankit/rls/RowLevelSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.rls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowLevelSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowLevelSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankit/rls/RowLevelSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.rls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowLevelSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
