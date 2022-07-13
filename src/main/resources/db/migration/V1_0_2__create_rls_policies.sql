e80b5f760c9361d7d662c29f5c60a25d95da2660
ankitwasnik <ankit@emtech.com>
2022-07-13 15:18:47 +0530

    Fix user and tenant sequence

--- src/main/resources/db/migration/V1_0_2__create_rls_policies.sql
+++ src/main/resources/db/migration/V1_0_2__create_rls_policies.sql
@@ -1,6 +1,6 @@
 DROP policy if exists tenant_users_isolation_policy ON public.users;
 
 CREATE POLICY tenant_users_isolation_policy ON public.users
-	USING (tenant_id::text = current_setting('app.tenant_id', true));
+	USING (tenant_id = current_setting('app.tenant_id', true)::integer);
 
 ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
